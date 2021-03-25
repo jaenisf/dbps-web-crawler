72fdf5d298217627c9708e39d1a24296375e3c74
Florian Jänisch <florian.j99@gmail.com>
2021-03-25 21:32:13 +0100

    crawler improved

--- initialize.php
+++ initialize.php
@@ -16,7 +16,23 @@
 			  `url` mediumtext DEFAULT NULL,
 			  `time_stamp` timestamp NULL DEFAULT NULL,
 			  `title` char(255) DEFAULT NULL,
+			  `page_rank` float DEFAULT NULL,
 			  PRIMARY KEY (`id`)
+			) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;";
+	query($mysqli, $sql);
+	
+	$sql = "DROP TABLE IF EXISTS `link_refers_to_link`;";
+	query($mysqli, $sql);
+	
+	$sql = "CREATE TABLE IF NOT EXISTS `link_refers_to_link` (
+			  `id` int(11) NOT NULL AUTO_INCREMENT,
+			  `id_link_from` int(11) DEFAULT NULL,
+			  `id_link_to` int(11) DEFAULT NULL,
+			  PRIMARY KEY (`id`),
+			  UNIQUE KEY `id_link_from_id_link_to` (`id_link_from`,`id_link_to`),
+			  KEY `FK_link_refers_to_link_link_2` (`id_link_to`),
+			  CONSTRAINT `FK_link_refers_to_link_link` FOREIGN KEY (`id_link_from`) REFERENCES `link` (`id`),
+			  CONSTRAINT `FK_link_refers_to_link_link_2` FOREIGN KEY (`id_link_to`) REFERENCES `link` (`id`)
 			) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;";
 	query($mysqli, $sql);
 	
@@ -51,7 +67,10 @@
 			  `id_link` int(11) NOT NULL,
 			  `count` int(11) DEFAULT NULL,
 			  PRIMARY KEY (`id`),
-			  UNIQUE KEY `id_word_id_link` (`id_word`,`id_link`)
+			  UNIQUE KEY `id_word_id_link` (`id_word`,`id_link`),
+			  KEY `FK_word_is_in_link_link` (`id_link`),
+			  CONSTRAINT `FK_word_is_in_link_link` FOREIGN KEY (`id_link`) REFERENCES `link` (`id`),
+			  CONSTRAINT `FK_word_is_in_link_word` FOREIGN KEY (`id_word`) REFERENCES `word` (`id`)
 			) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;";
 	query($mysqli, $sql);
 	
@@ -63,7 +82,10 @@
 			  `id_word` int(11) DEFAULT NULL,
 			  `id_stop_word` int(11) DEFAULT NULL,
 			  PRIMARY KEY (`id`),
-			  UNIQUE KEY `id_word_id_stop_word` (`id_word`,`id_stop_word`)
+			  UNIQUE KEY `id_word_id_stop_word` (`id_word`,`id_stop_word`),
+			  KEY `FK_word_is_stop_word_word_2` (`id_stop_word`),
+			  CONSTRAINT `FK_word_is_stop_word_word` FOREIGN KEY (`id_word`) REFERENCES `word` (`id`),
+			  CONSTRAINT `FK_word_is_stop_word_stop_word` FOREIGN KEY (`id_stop_word`) REFERENCES `stop_word` (`id`)
 			) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;";
 	query($mysqli, $sql);
 	
--- test.php
+++ test.php
@@ -1,5 +1,5 @@
 <?php
-	set_time_limit(60*60);
+	set_time_limit(60*5);
 	#error_reporting(0);
 
 	include('connect.php');
@@ -7,6 +7,8 @@
 	include('query.php');
 	
 	include('initialize.php');
+	#$sql = "USE `dbps-web-crawler`;";
+	#query($mysqli, $sql);
 	
 	include('crawler.php');
 	
@@ -17,9 +19,12 @@
 		$sql = "SELECT * FROM `link` WHERE url = '$link';";
 		$result = $mysqli->query($sql);
 
-		if ($result->num_rows > 0) {
+		if ($result->num_rows > 0) 
+		{
 			return true;		
-		} else {
+		} 
+		else 
+		{
 			return false;
 		}
 	}
@@ -29,9 +34,12 @@
 		$sql = "SELECT * FROM `link` WHERE url = '$link' AND time_stamp > TIMESTAMP(DATE_SUB(CURRENT_TIMESTAMP, INTERVAL 24 HOUR));";
 		$result = $mysqli->query($sql);
 
-		if ($result->num_rows > 0) {
+		if ($result->num_rows > 0) 
+		{
 			return true;		
-		} else {
+		} 
+		else 
+		{
 			return false;
 		}
 	}
@@ -48,14 +56,42 @@
 		query($mysqli, $sql);
 	}
 	
+	function add_relation_link_refers_to_link_to_database($mysqli, $link_from, $link_to)
+	{
+		$sql = "SELECT (SELECT id FROM link WHERE url = '$link_from') AS id_link_from,
+					   (SELECT id FROM link WHERE url = '$link_to') AS id_link_to;";
+		$result = $mysqli->query($sql);
+		
+		if ($result->num_rows == 1) 
+		{
+			while($row = $result->fetch_assoc()) 
+			{
+				$id_link_from = $row["id_link_from"];
+				$id_link_to = $row["id_link_to"];
+			}
+			
+			$sql = "SELECT * FROM `link_refers_to_link` WHERE id_link_from = '$id_link_from' AND id_link_to = '$id_link_to';";
+			$result = $mysqli->query($sql);
+			
+			if ($result->num_rows == 0) 
+			{
+				$sql = "INSERT INTO `link_refers_to_link` (id_link_from, id_link_to) VALUES ($id_link_from, $id_link_to);";
+				query($mysqli, $sql);
+			}
+		}
+	}
+	
 	function check_if_word_is_in_database($mysqli, $word)
 	{
 		$sql = "SELECT * FROM `word` WHERE word = '$word';";
 		$result = $mysqli->query($sql);
 
-		if ($result->num_rows > 0) {
+		if ($result->num_rows > 0) 
+		{
 			return true;		
-		} else {
+		} 
+		else 
+		{
 			return false;
 		}
 	}
@@ -71,9 +107,12 @@
 		$sql = "SELECT * FROM `stop_word` WHERE stop_word = '$word';";
 		$result = $mysqli->query($sql);
 
-		if ($result->num_rows > 0) {
+		if ($result->num_rows > 0) 
+		{
 			return true;		
-		} else {
+		} 
+		else 
+		{
 			return false;
 		}
 	}
@@ -84,7 +123,8 @@
 					   (SELECT id FROM link WHERE url = '$link') AS id_link;";
 		$result = $mysqli->query($sql);
 		
-		if ($result->num_rows == 1) {
+		if ($result->num_rows == 1) 
+		{
 			while($row = $result->fetch_assoc()) 
 			{
 				$id_word = $row["id_word"];
@@ -102,7 +142,8 @@
 					   (SELECT id AS id_stop_word FROM stop_word WHERE stop_word = '$word') AS id_stop_word;";
 		$result = $mysqli->query($sql);
 
-		if ($result->num_rows == 1) {
+		if ($result->num_rows == 1) 
+		{
 			while($row = $result->fetch_assoc()) 
 			{
 				$id_word = $row["id_word"];
@@ -132,7 +173,7 @@
 ?>
 
 <?php
-	function crawl($mysqli, $link, $maxDepth)
+	function crawl($mysqli, $link, $link_from, $maxDepth)
 	{
 		if(filter_var($link, FILTER_VALIDATE_URL) AND (str_starts_with($link, 'http://') OR str_starts_with($link, 'https://')))
 		{
@@ -150,12 +191,21 @@
 				{
 					update_link_in_database($mysqli, $link, $title);
 					$boolean_crawl = true;
-				} else {
+				} 
+				else 
+				{
 					$boolean_crawl = false;
 				}
-			} else {
+			} 
+			else 
+			{
 				add_link_to_database($mysqli, $link, $title);
 				$boolean_crawl = true;
+			}
+			
+			if ($link_from != "")
+			{
+				add_relation_link_refers_to_link_to_database($mysqli, $link_from, $link);
 			}
 			
 			$words = crawler_get_words($crawler);
@@ -181,51 +231,56 @@
 				}
 			}
 			
-			echo "<hr>";
+			$links = crawler_get_links($crawler);
 			
-			if ($maxDepth >= 0 AND $boolean_crawl == true) {
-			
-				$links = crawler_get_links($crawler);
-				
-				/*echo "<pre>";
-				print_r($links);
-				echo "</pre>";*/
-				
-				if ($links != false AND !empty($links))
+			if ($links != false AND !empty($links))
+			{
+				foreach ($links as $link)
 				{
-					foreach ($links as $link)
-					{				
-						if (str_starts_with($link, 'http://') == false AND str_starts_with($link, 'https://') == false) 
-						{
-							if (str_starts_with($link, '/')) {
-								$link = ($crawler->base).$link;
-							} else {
-								$link = ($crawler->base)."/".$link;
-							}
-						}
-						
-						if (filter_var($link, FILTER_VALIDATE_URL)) {
-							crawl($mysqli, $link, $maxDepth - 1);
-						} else {
-							echo "Die URL des Links zum Crawlen konnte nicht korrekt umgewandelt werden!";
-						}
-						
-						echo "<hr>";
-						
-						/*wenn ($link in der Form "/...")
-						füge $link links "http://<hostname_von_$URL>" an
-						wenn ($link in der Form "...")
-						füge $link links
-						"http://<hostname_von_$URL>/<dir_von_$URL>/" an
-						wenn ($link in der DB nicht als besuchte Seite
-						gespeichert ist)
-						crawl ( $link );*/
+					if (check_if_link_is_in_database($mysqli, $link) == true)
+					{
+						add_relation_link_refers_to_link_to_database($mysqli, ($crawler->base), $link);
 					}
-				} else {
-					echo "Die maximale Rekursionstiefe wurde erreicht!";
 				}
 			}
-		} else {
+			
+			echo "<hr>";
+			
+			if ($maxDepth >= 0 AND $boolean_crawl == true AND $links != false AND !empty($links))
+			{				
+				foreach ($links as $link)
+				{				
+					if (str_starts_with($link, 'http://') == false AND str_starts_with($link, 'https://') == false) 
+					{
+						if (str_starts_with($link, '/')) 
+						{
+							$link = ($crawler->base).$link;
+						} 
+						else 
+						{
+							$link = ($crawler->base)."/".$link;
+						}
+					}
+					
+					if (filter_var($link, FILTER_VALIDATE_URL)) 
+					{
+						crawl($mysqli, $link, ($crawler->base), $maxDepth - 1);
+					} 
+					else 
+					{
+						echo "Die URL des Links zum Crawlen konnte nicht korrekt umgewandelt werden!";
+					}
+					
+					echo "<hr>";
+				}
+			} 
+			else 
+			{
+				echo "Die maximale Rekursionstiefe wurde erreicht!";
+			}
+		} 
+		else 
+		{
 			echo "Die URL muss als relativer Pfad mit dem Protokoll http oder https angegeben werden!";
 		}
 	}
@@ -236,9 +291,103 @@
 		<h2>Webcrawler</h2>
 		
 <?php
-	#crawl($mysqli, 'http://www.dhbw-heidenheim.de', 1);
-	#crawl($mysqli, 'https://de.wikipedia.org/wiki/Rainer_Kuhlen', 1);
-	crawl($mysqli, 'https://www.heidenheim.de', 1);
+	#crawl($mysqli, 'http://www.dhbw-heidenheim.de', "", 1);
+	#crawl($mysqli, 'https://de.wikipedia.org/wiki/Rainer_Kuhlen', "", 1);
+	#crawl($mysqli, 'https://www.heidenheim.de', "", 1);
+	
+    if (isset($_GET['mode'])) {
+        $mode = $_GET['mode'];
+    } else {
+        $mode = "search";
+        echo "The 'mode' argument is missing. Possible are 'search', 'add_link' and 'worker'. Using default 'search' ...";
+    } 
+	
+	if ($mode == 'search')
+    {
+?>
+	<form action="" method="POST" id="search_form" style="text-align: center; padding: 20px; border: 1px solid black;">
+		<input type="text" name="search" id="search" minlength="1" maxlength="50">
+		&emsp;
+		<input type="submit" value="Suchen" name="submit_search">
+	</form>
+<?php
+		if (isset($_POST['submit_search']))
+		{
+			if(trim($_POST['search']) != "")
+			{
+				$search_words = htmlspecialchars($_POST['search']); // Freie Stellen Feuerwehr Heidenheim
+				$search_words = text_to_words_array_with_count($search_words);
+				
+				/*echo "<pre>";
+				print_r($search_words);
+				echo "</pre>";*/
+				
+				$sql = "SELECT *, 
+							   COUNT(count) AS anzahl_verschiedene_woerter, 
+							   SUM(count) AS anzahl_vorkommen_suchwoerter
+						FROM word
+						LEFT JOIN word_is_in_link ON word.id = word_is_in_link.id_word
+						LEFT JOIN link ON word_is_in_link.id_link = link.id
+						WHERE ";
+				
+				foreach ($search_words as $word => $count)
+				{
+					$sql = $sql."word LIKE '%$word%' OR ";
+				}
+				
+				$sql = substr($sql, 0, strlen($sql) - 3);
+				
+				$sql = $sql."GROUP BY id_link 
+							 ORDER BY anzahl_verschiedene_woerter DESC, 
+								  anzahl_vorkommen_suchwoerter DESC
+							 LIMIT 20;";
+				
+				$result = $mysqli->query($sql);
+							   
+				if (empty($result)) echo $mysqli->error;
+
+				if ($result->num_rows > 0) {
+					echo "==> Top 20 Suchergebnisse<br><ul>";
+					
+					while($row = $result->fetch_assoc()) 
+					{
+						$url = $row["url"];
+						$title = $row["title"];
+						
+						if ($row["title"] != "")
+						{
+							$title = "(".$title.")";
+						}
+						
+						echo $url = "<li style='padding: 5px;'>
+										<a href='".$url."' style='color: #00B2C3;'>".$url."</a>
+										".$title."
+									</li>";
+					}
+					
+					echo "</ul>";
+				} else {
+					echo "Es konnten keine Suchergebnisse gefunden werden!";
+				}
+			}
+		}
+    }
+	else if ($mode == 'add_link')
+    {
+        
+    }
+    else if ($mode == "worker")
+    {
+        
+    }
+	else if ($mode == "test")
+    {
+        crawl($mysqli, 'https://www.heidenheim.de', "", 1);
+    }
+    else
+    {
+        echo "Error: Crawler mode [".$crawler_mode."] not supported.<br>";
+    }
 ?>
 
 	</body>
