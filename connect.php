ed2b91fb48035d2f96344bd1a124e715a5cafeaf
jaenisf <66373378+jaenisf@users.noreply.github.com>
2021-04-07 18:32:43 +0200

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,6 +1,6 @@
 <?php
 	// Create database connection
-	$mysqli = new mysqli('127.0.0.1', 'root@localhost', ' 077e5275ccba501e94c836c2921e744e1837502897f6ce72');
+	$mysqli = new mysqli('127.0.0.1', 'root', ' 077e5275ccba501e94c836c2921e744e1837502897f6ce72');
 	#$mysqli = new mysqli('dbps-web-crawler-mysql-db-do-user-7584574-0.b.db.ondigitalocean.com:25060', 'doadmin', 'h4lh95xgcub2p1mn');
 
 	if ($mysqli->connect_errno) 
