700c69c6edf995903a043977ec5d828dad2cabf9
jaenisf <66373378+jaenisf@users.noreply.github.com>
2021-04-07 20:17:47 +0200

    Update index.php

--- index.php
+++ index.php
@@ -439,7 +439,7 @@
 	else if ($mode == "test")
     {
         crawl($mysqli, 'https://www.heidenheim.de', "", 2);
-		crawl($mysqli, 'https://www.dhbw-heidenheim.de', "", 21);
+		#crawl($mysqli, 'https://www.dhbw-heidenheim.de', "", 21);
 		#crawl($mysqli, 'https://de.wikipedia.org/wiki/Rainer_Kuhlen', "", 2);
 		#crawl($mysqli, 'https://de.wikipedia.org/wiki/Haushund', "", 2);
 		#crawl($mysqli, 'https://www.vdi.de', "", 2);
